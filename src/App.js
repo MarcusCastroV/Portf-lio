231073bc4aafe980ae0c8965dcb94d4a88ef7086
Marcus <mvcv2016@gmail.com>
2023-03-11 13:42:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { HomeScreen } from 'screen'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'styles/themes'
+import { GlobalStyle } from 'styles'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HomeScreen />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
